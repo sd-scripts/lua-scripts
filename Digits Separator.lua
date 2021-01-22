2b48a9fa6d5ebca32855571b1bb3a6df66d37995
Stepanik537 <33102793+stepanik537@users.noreply.github.com>
2021-01-23 00:56:42 +0500

    version 1.0
    bugfix before update

--- Digits Separator.lua
+++ Digits Separator.lua
@@ -9,34 +9,51 @@ local ev = require('lib.samp.events')
 
 function replace(text)
     local function comma(n)
-        local v1,v2,v3 = string.match(n,'^([^%d]*%d)(%d*)(.-)$')
+        local v1, v2, v3 = string.match(n,'^([^%d]*%d)(%d*)(.-)$')
         return (v1 .. (v2:reverse():gsub('(%d%d%d)','%1.'):reverse()) .. v3)
     end
-    for S in string.gmatch(text, "%$%d+") do
-        text = string.gsub(text, S, comma(S))
+    
+    if text:match('%$%d+%.0') or text:match('%d+%.0%$') then 
+        text = text:gsub('%.0', '')
     end
-    for S in string.gmatch(text, "%d+%$") do
-        S = string.sub(S, 0, #S-1)
-        text = string.gsub(text, S, comma(S))
-    end
+
+    local list = {}
+    for S in text:gmatch('(%d+)%$') do table.insert(list, S) end
+    for S in text:gmatch('%$(%d+)') do table.insert(list, S) end
+    
+    for _, v in ipairs(list) do text = string.gsub(text, v, comma(v), 1) end
+        
     return text
 end
 
 function ev.onShowDialog(id, style, title, button1, button2, text)
-    if text:find('%$') then 
-        return {id, style, title, button1, button2, replace(text)}
-    end
+    if title:find('%$') or text:find('%$') then title = replace(title) end
+    if text:find('%$') then text = replace(text) end
+    return {id, style, title, button1, button2, text}
 end
 
 function ev.onShowTextDraw(id, data)
-    if data.text:find('%$') then 
-        return {id, replace(data.text)}
+    if data.text and data.text:find('%$') then 
+        data.text = replace(data.text)
+        return {id, data}
+    end
+end
+
+function ev.onTextDrawSetString(id, text)
+    if text:find('%$') then
+        return {id, replace(text)}
+    end
+end
+
+function ev.onDisplayGameText(style, time, text)
+    if text:find('%$') then 
+        return {style, time, replace(text)}
     end
 end
 
 function ev.onCreate3DText(idObject, color, position, distance, testLOS, attachedPlayerId, attachedVehicleId, textObject)
-    if textObject:find('$') then 
-        return {idObject, color, position, distance, testLOS, attachedPlayerId, attachedVehicleId, replace(textObject)}
+    if textObject and textObject:find('%$') then 
+        return {idObject, color, position, distance, testLOS, attachedPlayerId, attachedVehicleId, textObject and replace(textObject) or ''}
     end
 end
 
@@ -49,6 +66,6 @@ end
 function onScriptTerminate(LuaScript, quitGame)
     if LuaScript == thisScript() then
         sampAddChatMessage('[Digits Separator] {ffffff}Скрипт неожиданно прекратил работу. Если вы предполагаете, из-за чего это могло произойти,', 0xBBBBBB)
-        sampAddChatMessage('{ffffff}и это не выход из игры/перезагрузка скриптов, свяжитесь с нами в сообществе ВК: vk.me/sd_scripts', 0xBBBBBB)
+        sampAddChatMessage('{ffffff}и это не является выходом из игры/перезагрузка скриптов, свяжитесь с нами в сообществе ВК: vk.me/sd_scripts', 0xBBBBBB)
     end
 end
