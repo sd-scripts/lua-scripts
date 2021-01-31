b5d3f0b0e1166f5e44195a20c2226c5663caf1d6
Stepanik537 <33102793+stepanik537@users.noreply.github.com>
2021-01-31 21:06:38 +0500

    Release Version
    https://vk.com/sd_scripts?w=wall-155812447_505

--- Digits Separator.lua
+++ Digits Separator.lua
@@ -2,64 +2,61 @@ script_author('S&D Scripts')
 script_name('Digits Separator')
 script_version('1.0')
 script_description("[EN] Separator of numbers by digits. Simplifies the appearance of large numbers.\n[RU] Разделитель чисел по разрядам. Упрощает вид больших чисел.")
-script_dependencies('SAMPFUNCS; SampEvents')
+script_dependencies('SAMPFUNCS; SampEvents; Moonloader 0.26+')
 script_url('https://sd-scripts.ru')
 
 local ev = require('lib.samp.events')
 
-function replace(text)
+function formatter(text)
     local function comma(n)
         local v1, v2, v3 = string.match(n,'^([^%d]*%d)(%d*)(.-)$')
-        return (v1 .. (v2:reverse():gsub('(%d%d%d)','%1.'):reverse()) .. v3)
+        return (tonumber(n) == 0 and 0 or (v1 .. (v2:reverse():gsub('(%d%d%d)','%1.'):reverse()) .. v3))
     end
     
     if text:match('%$%d+%.0') or text:match('%d+%.0%$') then 
         text = text:gsub('%.0', '')
     end
 
-    local list = {}
-    for S in text:gmatch('(%d+)%$') do table.insert(list, S) end
-    for S in text:gmatch('%$(%d+)') do table.insert(list, S) end
-    
-    for _, v in ipairs(list) do text = string.gsub(text, v, comma(v), 1) end
-        
+    for S in text:gmatch('(%d+)%$') do text = string.gsub(text, S, comma(S), 1) end
+    for S in text:gmatch('%$(%d+)') do text = string.gsub(text, S, comma(S), 1) end
+   
     return text
 end
 
 function ev.onShowDialog(id, style, title, button1, button2, text)
-    if title:find('%$') or text:find('%$') then title = replace(title) end
-    if text:find('%$') then text = replace(text) end
+    if title:find('%$') or text:find('%$') then title = formatter(title) end
+    if text:find('%$') then text = formatter(text) end
     return {id, style, title, button1, button2, text}
 end
 
 function ev.onShowTextDraw(id, data)
     if data.text and data.text:find('%$') then 
-        data.text = replace(data.text)
+        data.text = formatter(data.text)
         return {id, data}
     end
 end
 
 function ev.onTextDrawSetString(id, text)
     if text:find('%$') then
-        return {id, replace(text)}
+        return {id, formatter(text)}
     end
 end
 
 function ev.onDisplayGameText(style, time, text)
     if text:find('%$') then 
-        return {style, time, replace(text)}
+        return {style, time, formatter(text)}
     end
 end
 
 function ev.onCreate3DText(idObject, color, position, distance, testLOS, attachedPlayerId, attachedVehicleId, textObject)
     if textObject and textObject:find('%$') then 
-        return {idObject, color, position, distance, testLOS, attachedPlayerId, attachedVehicleId, textObject and replace(textObject) or ''}
+        return {idObject, color, position, distance, testLOS, attachedPlayerId, attachedVehicleId, textObject and formatter(textObject) or ''}
     end
 end
 
 function ev.onServerMessage(color, text)
     if text:find('%$') then
-        return {color, replace(text)}
+        return {color, formatter(text)}
     end
 end
 
